dbeaeed5ca78ddf1bc9363aa495bdf7c33b7a072
macarena piaggio <mpiaggio@dc.uba.ar>
2019-10-08 11:51:44 -0300

    Replace knn.cpp

--- src/knn.cpp
+++ src/knn.cpp
@@ -21,9 +21,8 @@ Vector KNNClassifier::distance_to_row(Vector row)
 {	
 	//Convierto a dense matrix porque sparse no tiene rowwise y entre norm y for termina siendo peor
 	Matrix temp = MatrixXd(_X);
-	temp.rowwise() -= row.transpose();
-
-	Vector res = res.rowwise().norm();
+	temp.rowwise() -= (row.transpose()).eval();
+	Vector res = temp.rowwise().norm();
 	return res;
 }
 
@@ -36,28 +35,17 @@ double KNNClassifier::predict_row(Vector row)
 	vector<pair<double, int> > argsort;
 	for (int i = 0; i < dist.size(); ++i)
 	{
-	 	argsort[i].first = dist(i);
-	 	argsort[i].second = i;
+	 	pair<double,int> par;
+	 	par.first = dist(i);
+	 	par.second = i;
+	 	argsort.push_back(par);
 	}
 	sort(argsort.begin(), argsort.end());
-	cout << "anda argsort" << endl;
-
-	cout << "[ ";
-	for (int i = 0; i < argsort.size(); ++i)
-	{
-		cout << "(" << argsort[i].first << "," << argsort[i].second << "), "; 
-	}
-	cout << "]";
-
-	//Matrix kvecinos;
 	int pos = 0;
 	int neg = 0;
 	for (unsigned int i = 0; i < _n_neighbors; ++i)
 	{
 		int j = argsort[i].second;
-		//kvecinos.row(i) = _y(j,1);
-		//VERIFICAR QUE LOS VALORES DE _y SEAN 0(NEG) Y 1(POS)
-		cout << _y(j,1) << endl; 
 		if(_y(j,1) == POS)
 			pos++;
 		else
@@ -74,12 +62,10 @@ Vector KNNClassifier::predict(SparseMatrix X)
 {
     // Creamos vector columna a devolver
     auto ret = Vector(X.rows());
-    cout << "anda esto" << endl;
 
     for (unsigned k = 0; k < X.rows(); ++k)
     {
-        ret(k) = this->predict_row(X.row(k));
-		cout << "anda la iter " << k << endl;        
+        ret(k) = this->predict_row(X.row(k));        
     }
 
     return ret;
