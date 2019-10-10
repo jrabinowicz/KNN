13c9068f30c131131f906175d5ad438f72e05db0
Rodrigo Laconte <rlaconte@dc.uba.ar>
2019-10-09 21:12:40 -0300

    intentando subir presicion parte 8000

--- src/knn.h
+++ src/knn.h
@@ -10,10 +10,15 @@ public:
     void fit(SparseMatrix X, Matrix y);
 
     Vector predict(SparseMatrix X);
+
+    unsigned int vecinos();
+	MatrixXd dame_X();
+	Matrix dame_y();
 private:
 	unsigned int _n_neighbors;
 	SparseMatrix _X;
 	Matrix _y;
+	
 	double predict_row(Vector row);
 	Vector distance_to_row(Vector row);
 };
